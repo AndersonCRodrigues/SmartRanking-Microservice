c693da3b84101328f5aa8ce583ea40381be555fd
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-16 19:15:43 -0300

    feat: create player controller

--- api_gateway/src/player/player.controller.ts
+++ api_gateway/src/player/player.controller.ts
@@ -1,15 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import {
   ClientProxy,
   ClientProxyFactory,
   Transport,
 } from '@nestjs/microservices';
 import { config } from 'dotenv';
+import { IPlayer } from './interfaces/player.interface';
+import CreatePlayerDto from './dtos/create_player.dto';
 
 config();
 
 const { RABBITMQ_URL } = process.env;
-@Controller('player')
+@Controller('api/v1/players')
 export class PlayerController {
   private clienteAdminBackend: ClientProxy;
 
@@ -22,5 +30,11 @@ export class PlayerController {
         noAck: false,
       },
     });
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createPlayer(@Body() createPlayerDto: CreatePlayerDto): void {
+    this.clienteAdminBackend.emit('create-player', createPlayerDto);
   }
 }
