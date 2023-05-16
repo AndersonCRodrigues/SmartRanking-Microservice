679b29603558250ed463e27d9338030d132c65be
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-16 19:08:52 -0300

    initialize class player controller

--- api_gateway/src/player/player.controller.ts
+++ api_gateway/src/player/player.controller.ts
@@ -1,4 +1,26 @@
 import { Controller } from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { config } from 'dotenv';
 
+config();
+
+const { RABBITMQ_URL } = process.env;
 @Controller('player')
-export class PlayerController {}
+export class PlayerController {
+  private clienteAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clienteAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [RABBITMQ_URL],
+        queue: 'admin-backend',
+        noAck: false,
+      },
+    });
+  }
+}
