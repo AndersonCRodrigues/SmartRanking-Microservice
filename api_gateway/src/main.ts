16b3fa3cfd021cd6a91f210b0a81cc3407a4cc81
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-17 08:21:01 -0300

    implements global filter

--- api_gateway/src/main.ts
+++ api_gateway/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LogginInterceptor } from './interceptors/logging.interceptor';
 import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { AllExceptionsFilter } from './filters/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new LogginInterceptor(), new TimeoutInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3000);
 }
