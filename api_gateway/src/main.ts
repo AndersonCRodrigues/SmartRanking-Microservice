8a25ade0c060b79f6a4dd7458746e750117f64b1
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-17 08:20:34 -0300

    implements global interceptor

--- api_gateway/src/main.ts
+++ api_gateway/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogginInterceptor } from './interceptors/logging.interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new LogginInterceptor(), new TimeoutInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
