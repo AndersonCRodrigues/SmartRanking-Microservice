f9d3ccd59aaf04d0cb6966b177e02f5c7712e2f5
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-17 13:28:36 -0300

    feat: aws module and service

--- api_gateway/src/app.module.ts
+++ api_gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { PlayerModule } from './player/player.module';
 import { CategoryModule } from './category/category.module';
 import { ChallengeModule } from './challenge/challenge.module';
 import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
+import { AwsModule } from './aws/aws.module';
 
 @Module({
-  imports: [PlayerModule, CategoryModule, ChallengeModule, ProxyRMQModule],
+  imports: [PlayerModule, CategoryModule, ChallengeModule, ProxyRMQModule, AwsModule],
   controllers: [],
   providers: [],
 })
