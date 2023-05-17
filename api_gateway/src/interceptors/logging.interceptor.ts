db18cf9c848257af47fd671550f6a70221a3b58b
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-17 08:18:11 -0300

    feat: interceptors

---
+++ api_gateway/src/interceptors/logging.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LogginInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.log('Before...');
+    const now = new Date();
+
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`After... ${+Date.now() - +now}ms`)));
+  }
+}
