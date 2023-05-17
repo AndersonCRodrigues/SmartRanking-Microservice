21523e950f0acde34e4c248ac3d5b390a11184bd
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-17 08:16:19 -0300

    feat: category controller methods

--- api_gateway/src/category/category.controller.ts
+++ api_gateway/src/category/category.controller.ts
@@ -1,10 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import {
   ClientProxy,
   ClientProxyFactory,
   Transport,
 } from '@nestjs/microservices';
 import { config } from 'dotenv';
+import { Observable } from 'rxjs';
 
 config();
 
@@ -22,6 +23,29 @@ export class CategoryController {
         queue: 'admin-backend',
         noAck: false,
       },
+    });
+  }
+
+  @Post('categories')
+  @UsePipes(ValidationPipe)
+  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    this.clienteAdminBackend.emit('create-category', createCategoryDto);
+  }
+
+  @Get('categories')
+  getCategory(@Query('idCategory') _id: string): Observable<any> {
+    return this.clienteAdminBackend.send('get-categories', _id || '');
+  }
+
+  @Patch('categories/:id')
+  @UsePipes(ValidationPipe)
+  updateCategorie(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ): void {
+    this.clienteAdminBackend.emit('update-category', {
+      id,
+      category: updateCategoryDto,
     });
   }
 }
