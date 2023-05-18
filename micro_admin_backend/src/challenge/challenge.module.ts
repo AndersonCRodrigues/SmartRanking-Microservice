3046ab3af47a1ed609f1ac6434c109860a7e44d9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-18 11:29:27 -0300

    feat: category module and player module

--- micro_admin_backend/src/challenge/challenge.module.ts
+++ micro_admin_backend/src/challenge/challenge.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ChallengeController } from './challenge.controller';
 import { ChallengeService } from './challenge.service';
+import { PlayerModule } from 'src/player/player.module';
+import { CategoryModule } from 'src/category/category.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChallengeSchema } from './interfaces/challenge.schema';
+import { MatchSchema } from './interfaces/match.schema';
 
 @Module({
+  imports: [
+    PlayerModule,
+    CategoryModule,
+    MongooseModule.forFeature([
+      { name: 'Challenge', schema: ChallengeSchema },
+      { name: 'Match', schema: MatchSchema },
+    ]),
+  ],
   controllers: [ChallengeController],
-  providers: [ChallengeService]
+  providers: [ChallengeService],
 })
 export class ChallengeModule {}
