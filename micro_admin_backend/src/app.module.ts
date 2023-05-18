08fc7bf97e53eec947e0b4a48137c902569d7656
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-18 11:25:47 -0300

    feat: challenge module, controller and service

--- micro_admin_backend/src/app.module.ts
+++ micro_admin_backend/src/app.module.ts
@@ -3,12 +3,13 @@ import { CategoryModule } from './category/category.module';
 import { PlayerModule } from './player/player.module';
 import { config } from 'dotenv';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChallengeModule } from './challenge/challenge.module';
 
 config();
 
 const { MONGODB_URL } = process.env;
 @Module({
-  imports: [MongooseModule.forRoot(MONGODB_URL), CategoryModule, PlayerModule],
+  imports: [MongooseModule.forRoot(MONGODB_URL), CategoryModule, PlayerModule, ChallengeModule],
   controllers: [],
   providers: [],
 })
